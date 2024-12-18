6d0e80f6290d8b5342175f722deda62f3c9a42bb
zjl-mbp <jiayao.zhao@proton.me>
2024-12-18 13:40:21 +0800

    docs: Add default comments for plugin options

--- src/options.ts
+++ src/options.ts
@@ -1,4 +1,12 @@
 export interface Options {
+  /**
+   * relative paths to the directory to search for pages.
+   * @default "src/app"
+   */
   readonly entry?: string
+  /**
+   * specify the file name as the routing page file.
+   * @default "index.tsx"
+   */
   readonly index?: string
 }
